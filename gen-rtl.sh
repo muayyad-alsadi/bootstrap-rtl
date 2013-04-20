14e85e1e7e60b4bf63954c00d629d2485d1dfc62
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2013-04-20 18:30:13 +0300

    apply our rtl.css in the docs

--- gen-rtl.sh
+++ gen-rtl.sh
@@ -18,6 +18,14 @@ find . -name '*.rtl*.css' -print0 |
     s/((?:margin|border|padding)-width)\s*:\s*(thin|medium|thick|inherit|auto|[\.\d]+(?:em|ex|px|in|cm|mm|pt|pc|%)?)\s+(thin|medium|thick|inherit|auto|[\.\d]+(?:em|ex|px|in|cm|mm|pt|pc|%)?)\s+(thin|medium|thick|inherit|auto|[\.\d]+(?:em|ex|px|in|cm|mm|pt|pc|%)?)\s+(thin|medium|thick|inherit|auto|[\.\d]+(?:em|ex|px|in|cm|mm|pt|pc|%)?)/${1}: ${2} ${5} ${4} ${3}/gi;
     s/((?:margin|border|padding)-style)\s*:\s*(none|hidden|dotted|dashed|solid|double|groove|ridge|inset|outset|inherit|auto)\s+(none|hidden|dotted|dashed|solid|double|groove|ridge|inset|outset|inherit|auto)\s+(none|hidden|dotted|dashed|solid|double|groove|ridge|inset|outset|inherit|auto)\s+(none|hidden|dotted|dashed|solid|double|groove|ridge|inset|outset|inherit|auto)/${1}: ${2} ${5} ${4} ${3}/gi;'
 
+find . -name '*.html' -print0 | xargs -0 perl -i -lwpe '
+    s!(<html [^>]*)dir=\S*\s*!$1!g;
+    s!<html !<html dir="rtl" !g;
+    s!(href="[^"]*?)(\.rtl|\.rtl\.min|\.min\.rtl)\.css!$1.css!g;
+    s!(href="\S*)\.css!$1.rtl.css!g;
+    s!\.min\.rtl\.css!.rtl.min.css!g;
+'
+
 # we either flip the js file (and maintain .rtl.js files) or just patch the following known js bugs
 perl -i -wlp0e '
     s!(}\s*\.tooltip\.(?:top|bottom) \.tooltip-arrow\s*{[^}]*)right:\s*50%;!$1left:50%;!gm;
